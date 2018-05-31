03a7cee0e24dd3b11e56887a111ba02973bd9350
luka <father.luka@gmail.com>
2018-05-31 21:16:01 +0200

    message

---
+++ player.py
@@ -0,0 +1,31 @@
+import os.path
+import pafy
+import urllib.request
+import urllib.parse
+import re
+import os
+import subprocess, signal
+
+file = 'NowOnAir/NowOnAir.txt'
+
+def player():
+    NowOnAir = open(file).readline()
+    NowOnAir = NowOnAir[7:]
+    with open('NowOnAir/NowOnAirOBS.txt', 'w') as NowOnAirOBS:
+        NowOnAirOBS.write(NowOnAir)
+    p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    out = out.decode('utf-8')
+    for line in out.splitlines():
+        if 'vlc' in line:
+            pid = int(line.split(None, 1)[0])
+            os.kill(pid, signal.SIGKILL)
+    query_string = urllib.parse.urlencode({"search_query": NowOnAir})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    link = ('http://www.youtube.com/watch?v=' + search_results[0])
+    videoPafy = pafy.new(link)
+    best = videoPafy.getbestvideo()
+    videompv = best.url
+    print (best.resolution)
+    subprocess.Popen(['cvlc', '--play-and-exit', '--no-video-title', videompv])
---
+++ visualradio.py
@@ -0,0 +1,28 @@
+from player import player
+import pyinotify
+
+class FileWatcher:
+    notifier = None
+
+    def start_watch(self, dir, callback):
+        wm = pyinotify.WatchManager()
+        self.notifier = pyinotify.Notifier(wm, EventProcessor(callback))
+        mask = (pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_DELETE
+                | pyinotify.IN_DELETE_SELF | pyinotify.IN_MOVED_FROM | pyinotify.IN_CLOSE_WRITE
+                | pyinotify.IN_MOVED_TO)
+        wdd = wm.add_watch(dir, mask, rec=True)
+        while True:
+            self.notifier.process_events()
+            if self.notifier.check_events():
+                self.notifier.read_events()
+
+class EventProcessor(pyinotify.ProcessEvent):
+    def __init__(self, callback):
+        self.event_callback = callback
+
+    def process_IN_CLOSE_WRITE(self, event):
+        player()
+        print('in close_Write')
+
+f = FileWatcher()
+f.start_watch(r'NowOnAir/NowOnAir.txt/', None)
